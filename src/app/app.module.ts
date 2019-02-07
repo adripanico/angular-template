d031649a9c00789461361c319a3c3916e78e45eb
Adrián Caballero <adrian@epiclabs.io>
2019-02-07 11:05:40 +0100

    Upgrading to Angular 7 and adding gesture support with hammerjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeModule } from '@app/features/home/home.module';
 import { SharedModule } from '@app/shared/shared.module';
 
+import 'hammerjs';
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/app.component';
 import { CoreModule } from '@app/core/core.module';
+import { GestureConfig } from '@angular/material/core';
 
 @NgModule({
   imports: [
@@ -24,7 +26,12 @@ import { CoreModule } from '@app/core/core.module';
     // app
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: GestureConfig,
+    }
+  ],
   bootstrap: [AppComponent],
   declarations: [AppComponent],
 })

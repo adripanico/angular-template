1812d2635a03c2f02707c0538aace0edd3aa28b5
Adrián Caballero <adrian@epiclabs.io>
2020-05-04 16:05:14 +0200

    Upgrading to Angular Material 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeModule } from '@app/features/home/home.module';
 import { SharedModule } from '@app/shared/shared.module';
-
-import 'hammerjs';
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/app.component';
 import { CoreModule } from '@app/core/core.module';
-import { GestureConfig } from '@angular/material/core';
 
 @NgModule({
   imports: [
@@ -27,10 +24,6 @@ import { GestureConfig } from '@angular/material/core';
     AppRoutingModule,
   ],
   providers: [
-    {
-      provide: HAMMER_GESTURE_CONFIG,
-      useClass: GestureConfig,
-    }
   ],
   bootstrap: [AppComponent],
   declarations: [AppComponent],
